d25c2de8ca7a6e708ddb2901338d8a91988d76b0
Silvia Dorosz <silviadorosz@gmail.com>
2018-09-26 17:00:53 -0300

    inicio da fase 3

--- script/script.js
+++ script/script.js
@@ -6,12 +6,32 @@ button.addEventListener("click", function (e){
     e.preventDefault();
     //alert("Oi!");
     
+    const checkAll = document.querySelector(".btn-check-all");
+    checkAll.style.display = "flex";
+    
+    checkAll.addEventListener("click", function(ev){
+        ev.preventDefault();
+        list.style.textDecoration = "line-through"
+        list.style.color = "gray"
+    })
+    
+    // const deleteAll = document.querySelector(".btn-delete-all");
+    // deleteAll.style.display = "flex";
+
+    // checkAll.addEventListener("click", function(evn){
+    //     evn.preventDefault();
+    //     for(i = 0; i < list.length[i]; i++){
+            
+    //     }
+    // })
+
+
     if(input.value === "" || input.value === " "){
         return false;
     }
     //ADD LINHA
     const linha = document.createElement("li");
-
+    linha.className = "list-item";
 
     //INSERE TEXTO DO INPUT NA LINHA
     linha.innerHTML = (input.value);
@@ -24,21 +44,31 @@ button.addEventListener("click", function (e){
 
     //ADD CHECK NOS ITENS
     linha.addEventListener("click", function(e){
+        e.preventDefault();
         linha.style.textDecoration = "line-through"
-        linha.addEventListener("click", function(event){
+        linha.style.color = "gray"
+
+    })
+        const span = document.createElement("span");
+        //ADD CLASSE NO SPAN
+        span.className = "list-tarefas__span"
+        //ADD CLASSE BOTÃO NO SPAN DIRETO NO HTML
+        span.innerHTML= `<button class="list-tarefas__botao-excluir">x</button>`
+
+        span.addEventListener("click", function(event){
             event.preventDefault();
+            //DELETA LINHA QUANDO CLICA NO X
             linha.remove()
         })
-    })
-
-    const span = document.createElement("span");
-    span.className = "list-tarefas__span"
-    span.innerHTML= `<button class="list-tarefas__botao-excluir">x</button>`
-    console.log(span)
-
-    linha.appendChild(span)    
-
-
+        // ADD SPAN NA LINHA   
+        linha.appendChild(span)    
 
 })
 
+
+
+    
+
+    
+
+   
