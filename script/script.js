f1ba1817cfa033f8d57441e40ec5b61d2d6cdcb8
Silvia Dorosz <silviadorosz@gmail.com>
2018-09-27 17:05:51 -0300

    Adicao de media query

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 const button = document.querySelector(".btn-tarefas");
 const input = document.querySelector(".form__input");
 const list = document.querySelector(".list-tarefas");
-const checkAll = document.querySelector(".btn-check-all");
-const deleteAll = document.querySelector(".btn-delete-all");
+// const checkAll = document.querySelector(".btn-check-all");
+// const deleteAll = document.querySelector(".btn-delete-all");
+const checkAll = document.querySelector(".checkbox-check-all");
+const deleteAll = document.querySelector(".checkbox-delete-all");
 const boxAll =  document.querySelector(".box-all");
 
 
@@ -12,6 +14,7 @@ button.addEventListener("click", function(e) {
 
 
     if (input.value.length === 0 || !input.value.trim()) {
+        alert ("Insira uma tarefa!");
         return false;
     }
     //ADD LINHA
@@ -30,14 +33,15 @@ button.addEventListener("click", function(e) {
     //ADD CHECK NOS ITENS
     linha.addEventListener("click", function(e) {
         e.preventDefault();
-        linha.style.textDecoration = "line-through"
-        linha.style.color = "gray"
+        
+        //INVERTE AS COISAS ATRAVÉS DA CLASSE QUE PEGOU NO CSS
+        this.classList.toggle("checked");
 
     })
     const span = document.createElement("span");
     //ADD CLASSE NO SPAN
     span.className = "list-tarefas__span"
-        //ADD CLASSE BOTÃO NO SPAN DIRETO NO HTML
+    //ADD CLASSE BOTÃO NO SPAN DIRETO NO HTML
     span.innerHTML = `<button class="list-tarefas__botao-excluir">x</button>`
 
     span.addEventListener("click", function(event) {
@@ -49,27 +53,26 @@ button.addEventListener("click", function(e) {
     linha.appendChild(span)
 
     //PARA VISUALIZAR OS BOTÕES DENTRO DO OUTRO BOTÃO
-    // deleteAll.style.display = "flex";
-    // checkAll.style.display = "flex";
-
     boxAll.style.display = "flex";
-    boxAll.style.justifyContent = "space-evenly";
+    boxAll.style.justifyContent = "space-around";
 })
 
-checkAll.addEventListener("click", function(ev) {
-    ev.preventDefault();
-    //ADD LINHA
+checkAll.addEventListener("click", function() {
     const item = document.querySelectorAll(".list-item");
     //FOR PARA RISCAR E MUDAR COR
     for (let checado of item) { // mesma coisa que for normal [array]
-        checado.style.textDecoration = "line-through"
-        checado.style.color = "gray"
+        
+        //INVERTE AS COISAS ATRAVÉS DA CLASSE QUE PEGOU NO CSS
+        checado.classList.toggle("checked");
     }
 })
 // EVENTO CLICK PARA REMOVER HTML E LIMPAR ESTILOS
-deleteAll.addEventListener("click", function(evn) {
-    evn.preventDefault();
+deleteAll.addEventListener("click", function() {
     list.innerHTML = ""
     //REMOVER O ESTILO TODA VEZ QUE DELETA
     list.removeAttribute("style");
+    boxAll.style.display = "none";
+    checkAll.checked = false;
+    this.checked = false;
 })
+
