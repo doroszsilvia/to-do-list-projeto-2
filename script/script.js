f1c6b603d362bc487cff66a6575a82afa6feb974
Silvia Dorosz <silviadorosz@gmail.com>
2018-09-27 09:13:00 -0300

    finalizacao da quarta fase

--- script/script.js
+++ script/script.js
@@ -1,32 +1,16 @@
 const button = document.querySelector(".btn-tarefas");
 const input = document.querySelector(".form__input");
 const list = document.querySelector(".list-tarefas");
+const checkAll = document.querySelector(".btn-check-all");
+const deleteAll = document.querySelector(".btn-delete-all");
 
-button.addEventListener("click", function (e){
+
+button.addEventListener("click", function(e) {
     e.preventDefault();
     //alert("Oi!");
-    
-    const checkAll = document.querySelector(".btn-check-all");
-    checkAll.style.display = "flex";
-    
-    checkAll.addEventListener("click", function(ev){
-        ev.preventDefault();
-        list.style.textDecoration = "line-through"
-        list.style.color = "gray"
-    })
-    
-    // const deleteAll = document.querySelector(".btn-delete-all");
-    // deleteAll.style.display = "flex";
-
-    // checkAll.addEventListener("click", function(evn){
-    //     evn.preventDefault();
-    //     for(i = 0; i < list.length[i]; i++){
-            
-    //     }
-    // })
 
 
-    if(input.value === "" || input.value === " "){
+    if (input.value.length === 0 || !input.value.trim()) {
         return false;
     }
     //ADD LINHA
@@ -38,37 +22,50 @@ button.addEventListener("click", function (e){
 
     //ADD LINHA DENTRO DA LISTA
     list.appendChild(linha);
-    
+
     //LIMPA INPUT AO ENVIAR INPUT
     input.value = "";
 
     //ADD CHECK NOS ITENS
-    linha.addEventListener("click", function(e){
+    linha.addEventListener("click", function(e) {
         e.preventDefault();
         linha.style.textDecoration = "line-through"
         linha.style.color = "gray"
 
     })
-        const span = document.createElement("span");
-        //ADD CLASSE NO SPAN
-        span.className = "list-tarefas__span"
+    const span = document.createElement("span");
+    //ADD CLASSE NO SPAN
+    span.className = "list-tarefas__span"
         //ADD CLASSE BOTÃO NO SPAN DIRETO NO HTML
-        span.innerHTML= `<button class="list-tarefas__botao-excluir">x</button>`
+    span.innerHTML = `<button class="list-tarefas__botao-excluir">x</button>`
 
-        span.addEventListener("click", function(event){
+    span.addEventListener("click", function(event) {
             event.preventDefault();
             //DELETA LINHA QUANDO CLICA NO X
             linha.remove()
         })
         // ADD SPAN NA LINHA   
-        linha.appendChild(span)    
+    linha.appendChild(span)
+
+
+    deleteAll.style.display = "inline-block";
+    checkAll.style.display = "inline-block";
+})
+
+checkAll.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    const item = document.querySelectorAll(".list-item");
+    for (let checado of item) { // mesma coisa que for normal [array]
+        checado.style.textDecoration = "line-through"
+        checado.style.color = "gray"
+
+    }
 
 })
 
+deleteAll.addEventListener("click", function(evn) {
+    evn.preventDefault();
+    list.innerHTML = ""
+    list.removeAttribute("style")
 
-
-    
-
-    
-
-   
+})
